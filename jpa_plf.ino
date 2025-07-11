86a46cf102608d792c52dc018dab29b26bd1a02b
SqrtKxah <0075304@academico.ifmg.edu.br>
2025-07-11 18:14:22 -0300

    Coisas

--- jpa_plf.ino
+++ jpa_plf.ino
@@ -1,4 +1,3 @@
-#include <ArduPID.h>
 #include <math.h>
 
 #define left_spd_pin 3
@@ -7,26 +6,19 @@
 #define left_mtr_back 52
 #define right_mtr_forward 51
 #define right_mtr_back 50
-const double pi = 3.14159;
 double input = 0;
 double output = 0;
-double setpoint = 0;
-double kp = 4, ki = 0.0, kd = .0;
+double last = 0;
+int first_run = 1;
 const double sensor_threshold = 900;
 
 const double max_spd = 255;
+const double max_turn_spd = 155;
 
-int sensor_pins[4] = { 22, 23, 25, 19};                       // pinos
-float sensor_values[4] = { -pi / 3, -pi / 4, pi / 4, pi / 3 };  // valores de peso agregados aos sensores
-
-ArduPID speed_pid;
-
-int invert(int num) {
-  return pow(num, 0) - num;
-}
+int sensor_pins[4] = { 22, 23, 25, 19};                  // pinos
+float sensor_values[4] = { -2, -1, 1, 2 };  // valores de peso agregados aos sensores
 
 void setup() {
-  Serial.begin(1200);
   pinMode(20, OUTPUT);
   digitalWrite(20,HIGH);
   pinMode(left_mtr_forward, OUTPUT);
@@ -35,6 +27,7 @@ void setup() {
   pinMode(right_mtr_back, OUTPUT);
   pinMode(left_spd_pin, OUTPUT);
   pinMode(right_spd_pin, OUTPUT);
+
   for (int i = 0; i <= 3; i++) {
     pinMode(sensor_pins[i], INPUT);
   }
@@ -43,10 +36,7 @@ void setup() {
   digitalWrite(right_mtr_forward, LOW);
   digitalWrite(left_mtr_back, HIGH);
   digitalWrite(right_mtr_back, HIGH);
-  speed_pid.begin(&input, &output, &setpoint, kp, ki, kd);
-  speed_pid.setOutputLimits(-pi, pi);
-  speed_pid.setSampleTime(1);
-  speed_pid.start();
+
 }
 
 void loop() {
@@ -57,51 +47,31 @@ void loop() {
   // ler sensor, calcular soma de erro e calcular velocidade
   for (int i = 0; i <= 3; i++) {
     int s_input = digitalRead(sensor_pins[i]);
-    // input += (s_input > sensor_threshold) * sensor_values[i];
-    // input += sensor_values[i] * invert(s_input);
     input += sensor_values[i] * s_input;
-    // Serial.println(String("Sensor " + String(i, DEC) + " Valor " + String((sensor_values[i] * s_input), DEC)));
   }
 
-  speed_pid.compute();
+  if ((last == input) && (!first_run)) {
+    return;
+  }
 
-  const double alpha = cos(output);
+  if ((input == 0) && (last != 1)) {
+    input = last;
+  }
 
-  // direção de motor dependendo do alpha
+  const double alpha = min(max(input, -1), 1);
 
-  left_spd -= min(-min(output, 0), 1) * alpha * max_spd;
-  right_spd -= min(max(output, 0), 1) * alpha * max_spd;
-
-  // if (fabs(output) > (pi / 2)) {
-  //   if (max(output, 0)) {
-  //     digitalWrite(left_mtr_forward, HIGH);
-  //     digitalWrite(right_mtr_forward, LOW);
-  //     digitalWrite(left_mtr_back, LOW);
-  //     digitalWrite(right_mtr_back, HIGH);
-  //   } else {
-  //     digitalWrite(left_mtr_forward, LOW);
-  //     digitalWrite(right_mtr_forward, HIGH);
-  //     digitalWrite(left_mtr_back, HIGH);
-  //     digitalWrite(right_mtr_back, LOW);
-  //   }
-  // } else {
-  //     digitalWrite(left_mtr_forward, HIGH);
-  //     digitalWrite(right_mtr_forward, HIGH);
-  //     digitalWrite(left_mtr_back, LOW);
-  //     digitalWrite(right_mtr_back, LOW);
-  // }
-  if (fabs(output) > (pi / 2)) {
-    if (max(output, 0) == 0) {
-      digitalWrite(left_mtr_forward, LOW);
-      digitalWrite(right_mtr_forward, HIGH);
-      digitalWrite(left_mtr_back, HIGH);
-      digitalWrite(right_mtr_back, LOW);
-      // Serial.println("HARD RIGHT");
-    } else {
+  if (fabs(input) != 0) {
+    if (max(input, 0) == 0) {
       digitalWrite(left_mtr_forward, HIGH);
       digitalWrite(right_mtr_forward, LOW);
       digitalWrite(left_mtr_back, LOW);
       digitalWrite(right_mtr_back, HIGH);
+      // Serial.println("HARD RIGHT");
+    } else {
+      digitalWrite(left_mtr_forward, LOW);
+      digitalWrite(right_mtr_forward, HIGH);
+      digitalWrite(left_mtr_back, HIGH);
+      digitalWrite(right_mtr_back, LOW);
       // Serial.println("HARD LEFT");
     }
   } else {
@@ -112,11 +82,15 @@ void loop() {
       // Serial.println("HARD FORWARD");
   }
 
+  // direção de motor dependendo do alpha
+
+  left_spd -= 1 - (-min(alpha, 0)) * max_turn_spd;
+  right_spd -= 1 - max(alpha, 0) * max_turn_spd;
+
   // atualizar velocidade
   analogWrite(left_spd_pin, left_spd);
   analogWrite(right_spd_pin, right_spd);
 
-  // Serial.println(output);
-  // Serial.println(String("Entrada" + String(input, DEC)));
-  // Serial.println(String("Saida" + String(output, DEC)));
+  first_run = 0;
+  last = input;
 }
